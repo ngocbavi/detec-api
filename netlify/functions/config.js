5447f83c3d9084863ad27b5e457cba2a770e245e
ngocbavi <lkdzlk4@gmail.com>
2025-09-22 20:02:42 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: '7496878193:AAErroYryul7-mh1DQMxJCKj_S06Q4EdM_4',
+    token: '7496878193:AAFcFGnbVRl6dMBogloLL6gnsCuK4Swurlo',
     chat_id: '-4911847713',
     noti_token: '',
     noti_chat_id: ''
